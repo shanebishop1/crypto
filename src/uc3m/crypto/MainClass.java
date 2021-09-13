6e9c9f35acee8dfbc740f108f39e7717b93817ec
lukybil <lukybil@gmail.com>
2021-09-13 10:01:25 +0200

    src

--- src/uc3m/crypto/MainClass.java
+++ src/uc3m/crypto/MainClass.java
@@ -1,18 +1,18 @@
 package uc3m.crypto;
 
 import java.util.Scanner;
+import java.net.*;
 
 public class MainClass {
     public static void main(String[] args) {
         String [][] db = new String [10][2];
         db[5][0] = "Luky";
-        db[5][1] = "Bil";
+        db[5][1] = "Bily";
         System.out.print("Username: ");
         Scanner input = new Scanner(System.in);
         String username = input.nextLine();
         System.out.print("Password: ");
         String password = input.nextLine();
-
         boolean loggedIn = false;
         for (String[] data : db) {
             if (data[0] != null && data[1] != null) {
