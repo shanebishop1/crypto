6db0d02bd5b2b9812f1cc70b4ccb2e16d373e6ba
lukybil <lukybil@gmail.com>
2021-10-27 18:42:06 +0200

    added hmac

--- src/uc3m/crypto/server/model/Message.java
+++ src/uc3m/crypto/server/model/Message.java
@@ -1,18 +1,23 @@
 package uc3m.crypto.server.model;
 
+import uc3m.crypto.security.SHA;
+
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.util.Base64;
 import java.util.Date;
 
 public class Message implements Serializable {
     private String sender;
     private String content;
     private Date dateSent;
+    private String hmac;
 
     public Message(String sender, String content, Date dateSent) {
         this.sender = sender;
         this.content = content;
         this.dateSent = dateSent;
+        this.hmac = getHmacString();
     }
 
     public Message(String message) {
@@ -28,10 +33,23 @@ public class Message implements Serializable {
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
+        hmac = message.substring(message.indexOf("hmac") + 6,
+                message.indexOf("'", message.indexOf("hmac") + 6));
+        if (!checkHmac()) throw new SecurityException("Message integrity compromised");
     }
 
     @Override
     public String toString() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return "Message{" +
+                "sender='" + sender + '\'' +
+                ", content='" + content + '\'' +
+                ", dateSent='" + formatter.format(dateSent) + '\'' +
+                ", hmac='" + hmac + '\'' +
+                '}';
+    }
+
+    public String toStringWithoutHmac() {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return "Message{" +
                 "sender='" + sender + '\'' +
@@ -43,6 +61,13 @@ public class Message implements Serializable {
     public String toUIString() {
         SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
         return "(" + formatter.format(dateSent) + ")[" + sender + "]: " + content;
+    }
+
+    public String getHmacString() {
+        return Base64.getEncoder().encodeToString(SHA.digest(this.toStringWithoutHmac()));
+    }
+    public boolean checkHmac() {
+        return getHmacString().equals(hmac);
     }
 
     public String getSender() {
@@ -67,5 +92,13 @@ public class Message implements Serializable {
 
     public void setDateSent(Date dateSent) {
         this.dateSent = dateSent;
+    }
+
+    public String getHmac() {
+        return hmac;
+    }
+
+    public void setHmac(String hmac) {
+        this.hmac = hmac;
     }
 }
